063d8f4a07e588fa982665b6ea853f487f38f930
otreblan <otreblain@gmail.com>
2023-08-26 20:36:59 -0500

    feat: Spawn spheres

--- Assets/Scripts/SphereSpawner.cs
+++ Assets/Scripts/SphereSpawner.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class SphereSpawner : MonoBehaviour
 {
-    public Camera camera;
+    public Camera mainCamera;
+
+    public GameObject prefab;
+
+    public float speed = 1.0f;
+    public float distance = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(!camera)
-            camera = Camera.main;
+        if(!mainCamera)
+            mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Ray ray = mainCamera.ScreenPointToRay(touch.position);
+
+            GameObject newObject = Instantiate(prefab, ray.GetPoint(distance), Quaternion.identity);
+
+            newObject.transform.parent = transform;
+            newObject.GetComponent<Rigidbody>().velocity = ray.GetPoint(speed)-ray.origin;
+        }
     }
 }
